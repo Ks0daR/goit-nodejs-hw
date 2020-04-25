860c80c70e3ff813ff358e3ecb7d6a0212c9ce75
Ivan Shevchenko <rayserviz@gmail.com>
2020-04-25 16:23:38 +0300

    working on hw

--- contacts.js
+++ contacts.js
@@ -1,32 +1,38 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-const contactsPath = path.join(__dirname, './db/contacts.json');
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-console.log(contactsPath);
+
 
 const { promises: fsPromises } = fs;
 
 async function listContacts() {
-  const result = await fsPromises.readFile(contactsPath, 'utf8');
-  return JSON.parse(result);
+  const fileDB = await fsPromises.readFile(contactsPath, "utf8");
+  const result = JSON.parse(fileDB);
+  return result;
 }
 
 async function getContactById(contactId) {
   const fileDB = (await listContacts()) || [];
   const contactById = fileDB.find(({ id }) => id === contactId);
-  console.log(contactById);
+  return contactById;
 }
 
 async function removeContact(contactId) {
   const fileDB = (await listContacts()) || [];
   const newFileContent = fileDB.filter(({ id }) => id !== contactId);
   await fsPromises.writeFile(
-    './db/newContacts.json',
+    "./db/newContacts.json",
     JSON.stringify(newFileContent)
   );
 }
 
-// function addContact(name, email, phone) {
-//   // ...твой код
-// }
+async function addContact(name, email, phone) {
+  const fileDB = (await listContacts()) || [];
+  fileDB.push({ id: uuidv4(), name, email, phone });
+  await fsPromises.writeFile("./db/newContacts.json", JSON.stringify(fileDB));
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
 
+console.log(argv);
+
+// async function main() {
+//   const result = await contacts.addContact(
+//     "luci",
+//     "lusi@mail.com",
+//     "123-123-123"
+//   );
+// }
+
+function invokeAction({ _: action, id }) {
+  switch (action.toString()) {
+    case "list":
+      async function main() {
+        const result = await contacts.listContacts();
+        return console.log(result);
+      }
+      main();
+      break;
+
+    case "get":
+      console.log("di", id[1]);
+      break;
+
+    //   case 'add':
+    //     // ... name email phone
+    //     break;
+
+    //   case 'remove':
+    //     // ... id
+    //     break;
+
+    default:
+      console.log(action);
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
+
+// require("yargs").command(
+
+//   "list",
+//   "get full contact list",
+//   (yargs) => {},
+//   (argv) => {
+//     console.log("111");
+//   }
+// ).argv;
