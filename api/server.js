9415a648e1f507201244e982af6f97521d8d8775
Ivan Shevchenko <rayserviz@gmail.com>
2020-05-04 16:26:31 +0300

    working on hw

--- api/server.js
+++ api/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { contactsRouter } from './contacts/contacts.router';
+
 const PORT = 3000;
+const corsOptions = {
+  orgign: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
 
 export class Server {
   constructor() {
@@ -8,14 +16,24 @@ export class Server {
 
   start() {
     this.initServer();
-    // this.initMiddleware;
-    // this.initRoutes;
+    this.initMiddleware();
+    this.initRoutes();
     // this.controlError;
     this.startListening();
   }
 
   initServer() {
     this.server = express();
+  }
+
+  initMiddleware() {
+    this.server.use(express.json());
+    this.server.use(cors(corsOptions));
+    this.server.use(morgan('tiny'));
+  }
+
+  initRoutes() {
+    this.server.use('/api', contactsRouter);
   }
 
   startListening() {
