3f2e8ccaa293b93195731997b5c66dca44761912
Ivan Shevchenko <rayserviz@gmail.com>
2020-05-21 16:32:33 +0300

    error

--- api/helpers/imagemin.js
+++ api/helpers/imagemin.js
@@ -2,15 +2,35 @@ import imagemin from 'imagemin';
 import imageminJpegtran from 'imagemin-jpegtran';
 import imageminPngquant from 'imagemin-pngquant';
 import path from 'path';
+import fs from 'fs';
 
 export async function imgCompressor(req, res, next) {
   const { path: uncompressedFilePath, filename } = req.file;
   const FILE_DESTINATION = `${__dirname}../../../public/images`;
 
-  const file = imagemin([`../../tmp/${filename}`], {
+  const files = await imagemin([uncompressedFilePath], {
     destination: FILE_DESTINATION,
-    plugins: [imageminJpegtran(), imageminPngquant({ quality: [0.6, 0.8] })],
+    plugins: [
+      imageminJpegtran(),
+      imageminPngquant({
+        quality: [0.6, 0.8],
+      }),
+    ],
   });
+
+  console.log(files);
+
+  console.log(
+    fs.exists(uncompressedFilePath, (exists) => {
+      console.log(exists);
+    })
+  );
+
+  console.log(
+    fs.exists(FILE_DESTINATION, (exists) => {
+      console.log(exists);
+    })
+  );
 
   req.file.path = path.join(FILE_DESTINATION, filename);
 
--- api/helpers/multer.js
+++ api/helpers/multer.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, __dirname + '../../../tmp');
+    cb(null, path.join(__dirname, '../../tmp'));
   },
   filename: (req, file, cb) => {
     const { ext } = path.parse(file.originalname);
