2fc42814396c3852c21c00883f7199fd7f37452b
Ivan Shevchenko <rayserviz@gmail.com>
2020-07-14 14:54:01 +0300

    checked upload file

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import { contactsRouter } from './contacts/contacts.router';
-import { authRouter } from './auth/auth.router';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { contactsRouter } from "./contacts/contacts.router";
+import { authRouter } from "./auth/auth.router";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 8080;
 const corsOptions = {
-  orgign: 'http://localhost:3000',
+  orgign: "http://localhost:3000",
   optionsSuccessStatus: 200,
 };
 
@@ -30,15 +30,15 @@ export class Server {
   }
 
   initMiddleware() {
-    this.server.use(express.static('public'));
+    this.server.use(express.static("public"));
     this.server.use(express.json());
     this.server.use(cors(corsOptions));
-    this.server.use(morgan('tiny'));
+    this.server.use(morgan("tiny"));
   }
 
   initRoutes() {
-    this.server.use('/auth', authRouter);
-    this.server.use('/api', contactsRouter);
+    this.server.use("/auth", authRouter);
+    this.server.use("/api", contactsRouter);
   }
 
   async initDbConnect() {
@@ -47,7 +47,7 @@ export class Server {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
-      console.log('Database connection successful');
+      console.log("Database connection successful");
     } catch (err) {
       console.log(err);
       process.exit(1);
@@ -64,7 +64,7 @@ export class Server {
 
   startListening() {
     this.server.listen(PORT, () => {
-      console.log('Server started and listening on port: ', PORT);
+      console.log("Server started and listening on port: ", PORT);
     });
   }
 }
